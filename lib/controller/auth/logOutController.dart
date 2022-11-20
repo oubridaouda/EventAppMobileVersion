8e3bc1d48f799ae79f74c317a171fac86e27abb2
Oubri Daouda <oubridaouda@gmail.com>
2022-11-20 00:52:25 +0000

    feat: add token blacklist

--- lib/controller/auth/logOutController.dart
+++ lib/controller/auth/logOutController.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:event_mobile_app/allChangeNotifer/AllChangeNotifer.dart';
 import 'package:event_mobile_app/screen/pages/home.dart';
 import 'package:flutter/cupertino.dart';
@@ -6,10 +9,34 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class LogOutController {
   final storage = FlutterSecureStorage();
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+  Map tokenResponse = {};
+
+  Future addTokenToBlacklist() async {
+    //Use http package to do api call request
+    var client = http.Client();
+    const url = 'www.e.kossyam.com';
+
+    String? value = await storage.read(key: 'jwt');
+    // print(value);
+    //Add user token to blacklist
+    var response = await client
+        .post(Uri.https(url, 'fr/add-token-to-blacklist'), headers: {
+      HttpHeaders.authorizationHeader: 'Bearer $value',
+    });
+
+    if (response.statusCode == 200) {
+      tokenResponse = jsonDecode(response.body);
+      print(tokenResponse);
+    } else {
+      tokenResponse = jsonDecode(response.body);
+      print(tokenResponse);
+    }
+  }
 
   Future<void> googleLogOut(context) async {
     try {
@@ -33,8 +60,7 @@ class LogOutController {
     storage.deleteAll();
   }
 
-
-  facebookLogOut(context) async{
+  facebookLogOut(context) async {
     await FacebookAuth.instance.logOut();
     final logged = await SharedPreferences.getInstance();
     //if sing out successfully pass isLoggedIn to false
@@ -42,9 +68,9 @@ class LogOutController {
     await storage.deleteAll();
   }
 
-  Future logOut(context) async{
-    //Display loader before logout
-
+  Future logOut(context) async {
+    //Add token to blacklist
+    await addTokenToBlacklist();
     // Google logout
     await LogOutController().googleLogOut(context);
 
@@ -54,10 +80,9 @@ class LogOutController {
     //Facebook logout
     await LogOutController().facebookLogOut(context);
 
-    Provider.of<AllChangeNotifier>(context,listen: false).userIsLogged(false);
+    Provider.of<AllChangeNotifier>(context, listen: false).userIsLogged(false);
 
     Provider.of<AllChangeNotifier>(context, listen: false)
         .changePage(DrawerSection.loginPage);
   }
-
 }
